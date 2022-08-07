00943a6e4ae9054d1bb5ebdf99523a1a0c8c5c42
Kirito <halid.gundz@gmail.com>
2022-08-07 23:54:16 +0200

    HILLRENT

---
+++ HillRent/src/main/java/com/hillrent/HillRentApplication.java
@@ -0,0 +1,13 @@
+package com.hillrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HillRentApplication.class, args);
+	}
+
+}
