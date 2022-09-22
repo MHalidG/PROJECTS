3fcc37e868596a17bc42c958d9804db3ed6a2273
Kirito <halid.gundz@gmail.com>
2022-09-22 22:14:59 +0200

    MicroServicesStoodUp

---
+++ MicroServicesExProject/config/src/main/java/com/tpe/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//Bu server config server olacak
+@EnableConfigServer
+
+//Bu service discoverable bir servis olacak
+@EnableEurekaClient
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
